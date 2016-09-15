83ef49c523953a34cc96228b3efac3788af18ddf
Ruy Calderon <ruy.calderon@gmail.com>
2016-09-15 14:35:45 -0500

    Basic menu functionality complete

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
-function setDefaultSize(selector, args){
-	$(selector).css('font-size', args.defaultSize);
-	$(selector).css('width', args.defaultWidth - ((args.targetElementWidth - args.defaultWidth) / 4));
+function setDefaultSize(selector, defaultSize){
+	$(selector).css('font-size', defaultSize);
 }
 
 $(document).ready(function(event) {
 	console.log('test');
 
 	$('.main-header').find('.navbar-option').each(function(){
+		if($(this).prev().length){
+			var previousLeft = parseFloat($(this).prev().css('left').replace('px',''));
+			var divWidth = parseFloat($('.main-header').css('width').replace('px','')*0.19);
+			$(this).css('left', (previousLeft + divWidth).toString() + 'px');
+		}
 		$(this).mousemove(function(event){
-			var maxHeight = parseFloat($('.main-header').css('height').replace('px','')/2);
-			var originalWidth = parseFloat($('.main-header').css('width').replace('px','')) * 0.19;
-			var currentHalfWidth = parseFloat($(this).css('width').replace('px',''))/2;
+			var height = parseFloat($(this).css('height').replace('px',''));
+			var width = parseFloat($(this).css('width').replace('px',''));
+			var mouseLocationX = event.originalEvent.offsetX;
+			var mouseLocationY = event.originalEvent.offsetY;
 
 			var startFontSize = 32;
-			var endFontSize = 64; //default: 20
-			
-			var YFromCenter = Math.abs(event.originalEvent.offsetY - maxHeight);
-			var XFromCenter = Math.abs(event.originalEvent.offsetX - currentHalfWidth);
-			console.log(XFromCenter + ' ' + YFromCenter);
-			var YNormalized = 1.0-(YFromCenter / maxHeight);
-			var XNormalized = 1.0-(XFromCenter / currentHalfWidth);
-			var normalized = XNormalized*(YNormalized*0.5 + XNormalized*0.5);
+			var endFontSize = 48;
+			if(!$(event.target).hasClass('deadzone')){
+				var deadZoneX = parseFloat($(this).find('.deadzone').css('width').replace('px',''));
+				var deadZoneY = parseFloat($(this).find('.deadzone').css('height').replace('px',''));
 
-			var targetFontSize = normalized * (endFontSize - startFontSize) + startFontSize;
-			var targetDivSize = (1.0+normalized) * originalWidth;
+				var xMin = deadZoneX / 2;
+				var yMin = deadZoneY / 2;
 
-			$(this).css('font-size', targetFontSize);
-			$(this).css('width', targetDivSize);
-			$(this).siblings().each(function(){
-				var args = {'defaultSize': startFontSize, 'defaultWidth': originalWidth, 'targetElementWidth': targetDivSize};
-				setDefaultSize(this, args);
-			});
+				var x = Math.abs(mouseLocationX - width/2);
+				var y = Math.abs(mouseLocationY - height/2);
+
+				var xMax =  width / 2;
+				var yMax =  height / 2;
+
+				var ratioX = (x - xMin) / (xMax - xMin);
+				var ratioY = (y - yMin) / (yMax - yMin);
+
+				var ratioToUse = 1-((ratioX < 0) ? ratioY : ratioX);
+
+				// console.log('X: ' + parseInt(xMin) + '<' + parseInt(x) + '<' + parseInt(xMax));
+				// console.log('Y: ' + parseInt(yMin) + '<' + parseInt(y) + '<' + parseInt(yMax));
+				// console.log('C/M: ' + ratioToUse);
+
+				var targetFontSize = ratioToUse *(endFontSize - startFontSize) + startFontSize;
+				targetFontSize = (targetFontSize < startFontSize) ? startFontSize : (targetFontSize > endFontSize) ? endFontSize : targetFontSize;
+				$(this).css('font-size', targetFontSize);
+				$(this).siblings().each(function(){
+					setDefaultSize(this, startFontSize);
+				});
+			}
+			else{
+				$(this).siblings().each(function(){
+					setDefaultSize(this, startFontSize);
+				});	
+			}
 		});
 	});
 });
