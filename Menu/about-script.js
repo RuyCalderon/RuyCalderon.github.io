ea7716f53a10c5c8473977639cb6769a3de5cca6
Ruy Calderon <ruy.calderon@gmail.com>
2016-09-28 14:18:38 -0500

    Submenu working correctly

--- Menu/about-script.js
+++ Menu/about-script.js
@@ -1,3 +1,16 @@
-$(document).onload(function(){
-	
-})
+$(document).ready(function(){
+	$('.main-header').mousemove(function(event) {
+		//it is actually changing position, so this will be difficult
+		
+	});
+
+
+	$('.deadzone').mouseenter(function(event) {
+		$(this).parent().removeClass('start-state').addClass('end-state');
+		$(this).parent().siblings('.start-state').removeClass('start-state').addClass('end-state');
+	});
+	$('.deadzone').parent().mouseleave(function(event) {
+		$(this).removeClass('end-state').addClass('start-state');
+		$(this).siblings('.end-state').removeClass('end-state').addClass('start-state');
+	});
+});
